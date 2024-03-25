5305df2378003fdb4d548e8334898535fa43d735
Joseph Eiba <josepheiba@icloud.com>
2024-03-25 23:29:45 +0000

    task4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - Inserts a node as the left-child of another node
+ * @node: Pointer to the node to insert the left-child in
+ *
+ * Return: Pointer to the created node, or NULL on failure
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+    if (node == NULL)
+        return (0);
+
+    if (node->right == NULL && node->left == NULL)
+        return (1);
+    return (0);
+}
