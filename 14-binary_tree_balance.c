70417e95696d07eaf016c82567054bfddb3dcab5
kamal <kamalmiftah01@gmail.com>
2024-03-26 11:46:18 +0000

    Solving task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Inserts a node as the left-child of another node
+ * binary_tree_height0 - Inserts a node as the left-child of another node
  * @tree: Pointer to the node to insert the left-child in
  *
  * Return: Pointer to the created node, or NULL on failure
