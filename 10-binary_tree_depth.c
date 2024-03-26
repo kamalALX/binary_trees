8728f1404d71c95b367dd5af11484b8252a7f29d
kamal <kamalmiftah01@gmail.com>
2024-03-26 05:59:37 +0000

    Solving task 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ * @tree: Pointer to the node to insert the left-child in
+ *
+ * Return: Pointer to the created node, or NULL on failure
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+    size_t left_height;
+    size_t right_height;
+
+    if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+        return (0);
+
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
+
+    if (left_height >= right_height)
+        return (left_height + 1);
+    return (right_height + 1);
+}
