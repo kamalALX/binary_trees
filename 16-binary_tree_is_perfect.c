67aeeb2669cbab2fcaad5b96c35397c95ca128bf
Joseph Eiba <josepheiba@icloud.com>
2024-03-27 05:29:23 +0000

    betty

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -14,7 +14,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree->left == tree->right && tree->left == NULL)
 		return (1);
 
-	if(binary_tree_is_full(tree->left) && (binary_tree_is_full(tree->right)))
+	if (binary_tree_is_full(tree->left) && (binary_tree_is_full(tree->right)))
 		return (1);
 	return (0);
 }
