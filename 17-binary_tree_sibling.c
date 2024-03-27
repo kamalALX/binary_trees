bb95f0c84a8359cc3c93662d0f207da28bb98ac3
Joseph Eiba <josepheiba@icloud.com>
2024-03-27 00:29:54 +0000

    task17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - Inserts a node as the left-child of another node
+ * @node: Pointer to the node to insert the left-child in
+ *
+ * Return: Pointer to the created node, or NULL on failure
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+    if (node == node->parent->left)
+        return (node->parent->right);
+    return (node->parent->left);
+}
