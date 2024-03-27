e38b49efd102258baffaef47f7fb7a83708d1021
Joseph Eiba <josepheiba@icloud.com>
2024-03-27 05:08:07 +0000

    task14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,22 +1,22 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height0 - Inserts a node as the left-child of another node
+ * binary_tree_height - Inserts a node as the left-child of another node
  * @tree: Pointer to the node to insert the left-child in
  *
  * Return: Pointer to the created node, or NULL on failure
  */
 
-int binary_tree_height0(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int left_height;
-	int right_height;
+	size_t left_height;
+	size_t right_height;
 
-	if (tree == NULL)
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 
-	left_height = binary_tree_height0(tree->left);
-	right_height = binary_tree_height0(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	if (left_height >= right_height)
 		return (left_height + 1);
@@ -37,6 +37,14 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	return (binary_tree_height0(tree->left) - binary_tree_height0(tree->right));
+    if (tree->left == NULL && tree->right != NULL)
+    {
+        return (-binary_tree_height(tree->right) - 1);
+    }
+    else if (tree->right == NULL && tree->left != NULL)
+    {
+        return (binary_tree_height(tree->left) + 1);
+    }
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 
 }
