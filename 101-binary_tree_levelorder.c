4b1e9f25e3abeb13922b993b6f7c8667bd68a79e
kamal <kamalmiftah01@gmail.com>
2024-03-27 10:31:11 +0000

    doc: removing white space

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -18,7 +18,7 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 	int height, i;
 
 	if (tree == NULL || func == NULL)
-        return;
+		return;
 
 	height = binary_tree_height(tree);
 
