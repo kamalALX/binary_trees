e3194f65f661d0a68791311eb33c579bfa9b5975
Joseph Eiba <josepheiba@icloud.com>
2024-03-28 18:10:29 +0000

    fix 111

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -27,7 +27,8 @@ bst_t *bst_insert(bst_t **tree, int value)
 			return ((*tree)->right = binary_tree_node(*tree, value));
 		return (bst_insert(&((*tree)->right), value));
 	}
-	return (NULL);
+	else
+		return (*tree);
 }
 
 /**
