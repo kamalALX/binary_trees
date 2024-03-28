3589ffed64ae11d5f995964cd57127fd5fafde93
Joseph Eiba <josepheiba@icloud.com>
2024-03-28 00:54:11 +0000

    fix 110

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,45 +1,36 @@
 #include "binary_trees.h"
-
-void binary_tree_inorder1(const binary_tree_t *tree, int queue[], int *rear);
+#include <limits.h>
 
 /**
- * binary_tree_is_bst - checks if a binary tree is a valid Binary Search Tree
- * @tree: a pointer to the root node of the tree to check
- * Return: 1 if tree is a valid BST, and 0 otherwise
+ * binary_tree_is_bst - checks if a binary tree is full
+ * @tree: Pointer to the node to insert the left-child in
+ *
+ * Return: returns 0 if tree is NULL
+ */
+
+int binary_tree_is_bst_range(const binary_tree_t *tree, int min, int max)
+{
+    if (tree == NULL)
+        return (1);
+
+    if (tree->n < min || tree->n >= max)
+        return (0);
+
+    return (binary_tree_is_bst_range(tree->left, min, tree->n) &&
+            binary_tree_is_bst_range(tree->right, tree->n, max));
+}
+
+/**
+ * binary_tree_is_bst - checks if a binary tree is full
+ * @tree: Pointer to the node to insert the left-child in
+ *
+ * Return: returns 0 if tree is NULL
  */
 
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	int i;
-	int rear = 0;
-	int queue[1024];
+    if (tree == NULL)
+        return (0);
 
-	if (tree == NULL)
-		return (0);
-
-	binary_tree_inorder1(tree, queue, &rear);
-
-	for (i = 0; i < rear; i++)
-	{
-		if (queue[i] > queue[i + 1])
-			return (0);
-	}
-	return (1);
-}
-
-/**
- * binary_tree_inorder1 - goes through a binary tree using in-order traversal
- * @tree: a pointer to the root node of the tree to traverse
- * @queue: an integer array to store the node values
- * @rear: a pointer to an integer that represents the rear of the queue
- */
-
-void binary_tree_inorder1(const binary_tree_t *tree, int queue[], int *rear)
-{
-	if (tree == NULL)
-		return;
-
-	binary_tree_inorder1(tree->left, queue, rear);
-	queue[(*rear)++] = tree->n;
-	binary_tree_inorder1(tree->right, queue, rear);
+    return (binary_tree_is_bst_range(tree, INT_MIN, INT_MAX));
 }
