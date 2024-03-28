d84c398f80c2833ef6e285e3e0cd18f37b58bb78
Joseph Eiba <josepheiba@icloud.com>
2024-03-28 12:58:09 +0000

    121 test

---
+++ 121-avl_insert.c
@@ -0,0 +1,82 @@
+#include "binary_trees.h"
+
+/**
+ * bst_insert - checks if a binary tree is full
+ * @tree: Pointer to the node to insert the left-child in
+ * @value: Pointer to the node to insert the left-child in
+ *
+ * Return: returns 0 if tree is NULL
+ */
+
+bst_t *bst_insert(bst_t **tree, int value)
+{
+	if (tree == NULL)
+		return (NULL);
+
+	if (*tree == NULL)
+		return (*tree = binary_tree_node(*tree, value));
+	if (value < (*tree)->n)
+	{
+		if ((*tree)->left == NULL)
+			return ((*tree)->left = binary_tree_node(*tree, value));
+		return (bst_insert(&((*tree)->left), value));
+	}
+	else if (value > (*tree)->n)
+	{
+		if ((*tree)->right == NULL)
+			return ((*tree)->right = binary_tree_node(*tree, value));
+		return (bst_insert(&((*tree)->right), value));
+	}
+	return (NULL);
+}
+
+/**
+ * avl_insert - checks if a binary tree is full
+ * @tree: Pointer to the node to insert the left-child in
+ * @value: Pointer to the node to insert the left-child in
+ *
+ * Return: returns 0 if tree is NULL
+ */
+
+avl_t *avl_insert(avl_t **tree, int value)
+{
+	avl_t *node, *holdT;
+	int bf;
+	int number;
+
+	bf = 0;
+	node = bst_insert(tree, value);
+	if (node->parent != NULL && node->parent->parent != NULL)
+		bf = binary_tree_balance(node->parent->parent);
+
+	if (bf != -1 && bf != 0 && bf != 1)
+	{
+		if (node->parent->left == node && node->parent->parent->left == node->parent)
+			binary_tree_rotate_right(node->parent->parent);
+		else if (node->parent->right == node && node->parent->parent->right == node->parent)
+			binary_tree_rotate_left(node->parent->parent);
+		else if (node->parent->right == node && node->parent->parent->left == node->parent)
+		{
+			number = node->parent->n;
+			node->parent->n = node->n;
+			node->n = number;
+
+			holdT = node->parent->left;
+			node->parent->left = node;
+			node->parent->right = holdT;
+			binary_tree_rotate_right(node->parent->parent);
+		}
+		else if (node->parent->left == node && node->parent->parent->right == node->parent)
+		{
+			number = node->parent->n;
+			node->parent->n = node->n;
+			node->n = number;
+
+			holdT = node->parent->right;
+			node->parent->right = node;
+			node->parent->left = holdT;
+			binary_tree_rotate_left(node->parent->parent);
+		}
+	}
+	return (node);
+}
