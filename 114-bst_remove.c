ad3527eda277104d540302b6024b11a1adba8238
Joseph Eiba <josepheiba@icloud.com>
2024-03-28 07:55:06 +0000

    114 b3tty

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -31,17 +31,17 @@ bst_t *bst_search(const bst_t *tree, int value)
 
 int bst_min(bst_t *tree)
 {
-    int mini;
-    bst_t *node;
+	int mini;
+	bst_t *node;
 
-    node = tree;
+	node = tree;
 
-    while (node != NULL)
-    {
-        mini = node->n;
-        node = node->left;
-    }
-    return (mini);
+	while (node != NULL)
+	{
+		mini = node->n;
+		node = node->left;
+	}
+	return (mini);
 }
 
 /**
@@ -54,69 +54,69 @@ int bst_min(bst_t *tree)
 
 bst_t *bst_remove(bst_t *root, int value)
 {
-    bst_t *node;
-    int mini;
+	bst_t *node;
+	int mini;
 
-    if (root == NULL)
-        return (NULL);
+	if (root == NULL)
+		return (NULL);
 
-    node = bst_search(root, value);
+	node = bst_search(root, value);
 
-    if (node->left == NULL && node->right == NULL)
-    {
-        if (node->parent != NULL && node == node->parent->left)
-            node->parent->left = NULL;
-        else if (node->parent != NULL && node == node->parent->right)
-            node->parent->right = NULL;
-        free(node);
-        node = NULL;
-    }
-    else if (node->left == NULL || node->right == NULL)
-    {
-        if (node->parent != NULL && node == node->parent->left)
-        {
-            if (node->left != NULL)
-            {
-                node->parent->left = node->left;
-                node->left->parent = node->parent;
-            }
-            if (node->right != NULL)
-            {
-                node->parent->left = node->right;
-                node->right->parent = node->parent;
-            }
-        }
-        else if (node->parent != NULL && node == node->parent->right)
-        {
-            if (node->left != NULL)
-            {
-                node->parent->right = node->left;
-                node->left->parent = node->parent;
-            }
-            if (node->right != NULL)
-            {
-                node->parent->right = node->right;
-                node->right->parent = node->parent;
-            }
-        }
-        else if (node->parent == NULL && node->left != NULL)
-        {
-            root = node->left;
-            node->left->parent = NULL;
-        }
-        else if (node->parent == NULL && node->right != NULL)
-        {
-            root = node->right;
-            node->right->parent = NULL;
-        }
-        free(node);
-        node = NULL;
-    }
-    else if (node->left != NULL && node->right != NULL)
-    {
-        mini = bst_min(node->right);
-        root = bst_remove(root, mini);
-        node->n = mini;
-    }
-    return (root);
+	if (node->left == NULL && node->right == NULL)
+	{
+		if (node->parent != NULL && node == node->parent->left)
+			node->parent->left = NULL;
+		else if (node->parent != NULL && node == node->parent->right)
+			node->parent->right = NULL;
+		free(node);
+		node = NULL;
+	}
+	else if (node->left == NULL || node->right == NULL)
+	{
+		if (node->parent != NULL && node == node->parent->left)
+		{
+			if (node->left != NULL)
+			{
+				node->parent->left = node->left;
+				node->left->parent = node->parent;
+			}
+			if (node->right != NULL)
+			{
+				node->parent->left = node->right;
+				node->right->parent = node->parent;
+			}
+		}
+		else if (node->parent != NULL && node == node->parent->right)
+		{
+			if (node->left != NULL)
+			{
+				node->parent->right = node->left;
+				node->left->parent = node->parent;
+			}
+			if (node->right != NULL)
+			{
+				node->parent->right = node->right;
+				node->right->parent = node->parent;
+			}
+		}
+		else if (node->parent == NULL && node->left != NULL)
+		{
+			root = node->left;
+			node->left->parent = NULL;
+		}
+		else if (node->parent == NULL && node->right != NULL)
+		{
+			root = node->right;
+			node->right->parent = NULL;
+		}
+		free(node);
+		node = NULL;
+	}
+	else if (node->left != NULL && node->right != NULL)
+	{
+		mini = bst_min(node->right);
+		root = bst_remove(root, mini);
+		node->n = mini;
+	}
+	return (root);
 }
